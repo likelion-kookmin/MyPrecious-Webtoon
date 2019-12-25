5525c0c8f1581203ec71bb9a0590e40930b79688
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2019-12-26 03:08:16 +0900

    네이버 에피소드 크롤링 구현

--- DjangoProject/contentsApp/models.py
+++ DjangoProject/contentsApp/models.py
@@ -13,9 +13,16 @@ class Webtoon(models.Model):
 
     def __str__(self):
         s = [str(i) for i in self.cartoonists.all()]
-        # for i in self.cartoonists.all():
-        #     s.append()
         return f"{self.name} - {', '.join(s)} by {self.content_provider}"
+
+    def is_all_episode_free(self):
+        is_free = True
+        
+        for i in Episode.objects.get(webtoon=self):
+            if not i.isFree:
+                is_free = False
+                break
+        return is_free
 
 class Cartoonist(models.Model):
     image = models.ImageField(null=True)
@@ -47,10 +54,12 @@ class AgeRatingSystem(models.Model):
 
 class Episode(models.Model):
     webtoon = models.ForeignKey('Webtoon', on_delete=models.PROTECT)
-    image = models.ImageField()
+    image = models.TextField()
     number = models.IntegerField()
     title = models.TextField()
     created = models.DateField()
     isFree = models.BooleanField()
-
     url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.webtoon.name} {[self.number]} \"{self.title}\" ({self.created})"
