5bc3bb36d76f7bc155409ecd2a2fe033e48b718c
Aqudi <gjdigj@naver.com>
2019-12-28 18:24:36 +0900

    Cusotom User 모델 수정 및 kakao 유저 데이터 기반으로 Profile을 입력하도록 수정

--- DjangoProject/MypreciousWebtoon/settings.py
+++ DjangoProject/MypreciousWebtoon/settings.py
@@ -162,4 +162,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- DjangoProject/accountApp/models.py
+++ DjangoProject/accountApp/models.py
@@ -6,6 +6,27 @@ from django.dispatch import receiver
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
+    def create_kakao_user(self, user_pk, extra_data):
+        user = CustomUser.objects.get(pk=user_pk)
+        profile = user.profile
+
+        # kakao_profile을 기반으로 정보 입력
+        kakao_properties = extra_data.get("properties")
+        profile.nickname = kakao_properties.get('nickname')
+        profile.get_image_from_url(kakao_properties.get('thumbnail_image'))
+
+        kakao_account = extra_data.get("kakao_account")
+        profile.gender = 'M' if kakao_account.get('gender') == "male" else 'F'
+        age_range = kakao_account.get('age_range')
+        profile.age_range = age_range[:2] if age_range else age_range
+        print(age_range)
+        profile.date_of_birth = kakao_account.get('birthday')
+        profile.save()
+        user.save()
+
+        print(user.profile)
+        return user
+
     def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError('Users must have an email address')
@@ -59,27 +80,49 @@ class CustomUser(AbstractBaseUser):
 
 
 class Profile(models.Model):
-    MAIL = 1
-    FEMAIL = 2
     GENDER_CHOICES = (
-        (MAIL, "남자"),
-        (FEMAIL, "여자")
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    AGE_RANGE = (
+        (10, "10"),
+        (20, "20"),
+        (30, "30"),
+        (40, "40"),
+        (50, "50"),
+        (60, "60"),
     )
 
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     nickname = models.CharField(blank=True, null=True, max_length=30)
-    gender = models.BooleanField(blank=True, null=True, choices=GENDER_CHOICES)
+    gender = models.CharField(blank=True, null=True, max_length=1, choices=GENDER_CHOICES)
     image = models.ImageField(blank=True, null=True)
+    age_range = models.CharField(blank=True, null=True, max_length=2, choices=AGE_RANGE)
     date_of_birth = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.nickname} ({self.user.email})'
 
+    def get_image_from_url(self, url):
+        from django.core.files.base import ContentFile
+        import urllib3, os
 
-@receiver(post_save, sender=CustomUser)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+        http = urllib3.PoolManager()
+
+        file = http.request('GET', url).data
+        file = ContentFile(file)
+        filename = os.path.basename(url)
+        self.image.save(filename, file)
+
+    def get_values(self):
+        from django.forms.models import model_to_dict
+        return model_to_dict(self, fields=[field.name for field in self._meta.fields])
 
 
+# allauth adapter로 처리
+# @receiver(post_save, sender=CustomUser)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     instance.profile.save()
