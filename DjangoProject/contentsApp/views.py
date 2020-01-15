79c709239d6569254d6c32d91bb595fa695a8115
sehuikwon <sehui4011@hanmail.net>
2020-01-15 18:07:51 +0900

    사진안되는 네브바ㅜ

--- DjangoProject/contentsApp/views.py
+++ DjangoProject/contentsApp/views.py
@@ -15,9 +15,19 @@ from .forms import *
 
 # Create your views here.
 
+
 def webtoon_detail(request, id):
     webtoon = Webtoon.objects.get(pk=id)
-   
+    # if request.method == "POST":
+    #     form = CommentForm(request.POST)
+    #     if form.is_valid():
+    #         comment = form.save(commit=False)
+    #         comment.author = request.user
+    #         comment.post = post
+    #         comment.save()
+    #         return redirect('webtoon_detail', pk=id)
+    # else:
+    #     form = CommentForm()
     return render(request, 'webtoon_detail.html', {'webtoon': webtoon})
 
 def Rated(request):
