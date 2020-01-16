ae7db0602833d678717bb20a4ed525b7e5dc11ba
Jin <ilikesjysjy@gmail.com>
2020-01-16 20:45:30 +0900

    jin0

--- DjangoProject/MypreciousWebtoon/settings.py
+++ DjangoProject/MypreciousWebtoon/settings.py
@@ -157,9 +157,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'MypreciousWebtoon', 'static')
+]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
