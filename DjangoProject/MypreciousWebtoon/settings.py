62733da82c10a1aac73f834194944c5aaaf4d9d9
inth8204 <inth8204@gmail.com>
2020-01-17 17:19:14 +0900

    setting adjusting for web

--- DjangoProject/MypreciousWebtoon/settings.py
+++ DjangoProject/MypreciousWebtoon/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k3v_1sq#4x0=nhvvv@&zl=%ql)_tfx8qfoo(z21x$dphk-ioky'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','k3v_1sq#4x0=nhvvv@&zl=%ql)_tfx8qfoo(z21x$dphk-ioky')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
